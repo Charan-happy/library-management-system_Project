c0ebc7e8f632bdfc0416e30a24290a7f12dc0fff
Nagacharan <89054489+Charan-happy@users.noreply.github.com>
2022-07-19 14:25:14 +0530

    Add files via upload

---
+++ index.php
@@ -0,0 +1,24 @@
+<html>
+<head>
+	<title>Public Library System</title>
+</head>
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+<link rel="stylesheet" href="css/all.css">
+<style>
+	body
+	{
+
+		background-image: url(Images/Home.jpg);
+		 background-repeat: no-repeat;
+		  background-size:100% 100%;
+	}
+</style>
+<body>
+	<div class="h1">
+		<h1> <center><font size=15 color=#70F4F7> WELCOME   TO   PUBLIC   LIBRARY </font><center></h1>
+		</center>
+	</div>
+	
+	<button onclick="window.location.href = 'login.php'" class="submit_button submit_button1" style="float: right;">LOGIN</button>
+</body>
+</html>
